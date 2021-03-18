e55cf989517dae24a97f218ed98594bebb6e3eb7
Makarov ilya <74796903+Bakl9@users.noreply.github.com>
2021-03-18 11:37:48 +0300

    Update script.js
    this

--- script.js
+++ script.js
@@ -8,3 +8,48 @@ let user = new User ('Вася');
 
 alert (user.name);
 alert (this.isAdmin);
+
+
+
+// function User(name) {
+//     this.name = name;
+//     this.isAdmin = false;
+// }
+
+// let user = new User ('Вася');
+
+// alert (user.name);
+// alert (this.isAdmin);
+
+
+
+// console.log(this);
+
+
+// function myFunction() {
+//     console.log(this);
+// }
+
+// myFunction();
+
+
+// var dog = {
+//     name: "chester",
+//     breed: 'beagle',
+//     intro: function() {
+//         console.log(this);
+//     }
+// };
+
+// dog.intro();
+
+
+// let room = {
+//     name: "Vasya",
+//     Age: 30,
+//     intro: function () {
+//         console.log(this);
+//     }
+// };
+
+// room.intro();
